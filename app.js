4383998fed7199df15556b0a3b76412bf9f44814
iTrxpical <34778563+iTrxpical@users.noreply.github.com>
2018-06-02 08:18:05 +0100

    Update app.js

--- app.js
+++ app.js
@@ -16,6 +16,11 @@ client.on('message', message => {
 
     if (sender.bot) return;
     if (message.channel.type === 'dm') {
+        
+        let c = a.channels.get("449182354507038720")
+c.send("t")
+})
+        
         message.channel.send("**We can't read anything in DM's! Please go into a guild with me in and run the command `=info` for information about me!!!**")
         return;
     }
