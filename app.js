5b9886aa98e4ccd2973645723290ba7a80516d26
iTrxpical <34778563+iTrxpical@users.noreply.github.com>
2018-07-26 11:01:06 +0100

    Update app.js

--- app.js
+++ app.js
@@ -102,12 +102,7 @@ if (support) {
         .setDescription(message.content);
     return message.channel.send(embed);
 }
-    
-    if (message.channel.type === 'dm') {
-        message.channel.send("**We can't read anything in DM's! Please go into a guild with me in and run the command `=info` for information about me!!!**")
-        return;
-    }
-        
+            
     var guildid = message.guild.id
     
     let msg = message.content.toLowerCase();
